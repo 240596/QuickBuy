421d5e241b60c15fc64120fdcb55501102f6880d
marco.almeida <marco.almeida@coopersystem.com.br>
2020-02-13 14:46:43 -0300

    Criação das entidades

---
+++ QuickBuy.Dominio/Entidades/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace QuickBuy.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Decimal { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
